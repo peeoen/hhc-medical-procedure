43e3c30d0c3e51a991a8121131f00e795a09948f
Theerawat Burawat <theerawat.pon@gmail.com>
2019-10-11 01:15:09 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hhc-image-drawing';
+
+  cursors: Cursor[] = [
+    {
+      cx: 25,
+      cy: 25,
+      top: '10%',
+      left: '10%',
+    }
+  ];
+
+  @ViewChild('canvas', {static: false}) canvas: ElementRef;
+  @ViewChild('downloadLink', {static: false}) downloadLink: ElementRef;
+
+  print() {
+    html2canvas(document.querySelector('#main')).then(canvas => {
+      console.log(canvas);
+      const a = document.createElement('a');
+      a.href = canvas.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream');
+      a.download = 'somefilename.jpg';
+      a.click();
+
+  });
+  }
+
+  addCursor() {
+    this.cursors.push({
+      cx: 25,
+      cy: 25,
+    });
+  }
+
+  remove(c: Cursor) {
+    this.cursors = this.cursors.filter(x => x !== c);
+  }
+}
+
+export class Cursor {
+  top?: string;
+  left?: string;
+  cx?: number;
+  cy?: number;
 }
